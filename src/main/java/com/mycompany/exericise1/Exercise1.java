2d78f3cd74499bb0f37cf9a1e6626934ce38eeab
spaniard95 <panagiotis.spanos95@gmail.com>
2020-03-02 18:46:08 +0200

    has errors still

--- src/main/java/com/mycompany/exericise1/Exercise1.java
+++ src/main/java/com/mycompany/exericise1/Exercise1.java
@@ -3,6 +3,7 @@ package com.mycompany.exericise1;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Exercise1 {
 
     /**
@@ -11,16 +12,29 @@ public class Exercise1 {
     public static void main(String[] args) {
         Random random = new Random();
         GameData game;
-        try (Scanner scan = new Scanner(System.in)) {
+        String userAnswer;
+        Scanner scan = new Scanner(System.in); 
             do{
             game=new GameData(random.nextInt(101)+1);
+             do{
+                 do{
+                 game.messege();
+                 userAnswer=scan.next(); //τρυ εδω
+                 }while(game.answerNumError(userAnswer));    //οταν η εισοδος ειναι αποδεκτη δηλ ειναι ιντ και αναμεσα στα ορια θα βγει απο το λοοπ
+                 
+                 game.playGame();
+                 
+            }while(game.getSituation().equals("επιτυχια"));
             do{
-                
-            }while(game.found());
+               game.messege(); 
+               userAnswer=scan.next();
+           }while(game.answerError(userAnswer));
             
-           }while(game.getGameEnd());
-            }
+          }while(game.isGameEnd());
+            
 
         }
     }
+ 
+
 
--- src/main/java/com/mycompany/exericise1/GameData.java
+++ src/main/java/com/mycompany/exericise1/GameData.java
@@ -3,49 +3,112 @@ package com.mycompany.exericise1;
 
 
 public class GameData {
-    private String userNum;
-    private int randomNum,round,inf,sup;
+   
+    private String userAnswer;
+    private final int randomNum;
+    private int userNum,round,inf,sup;
     private boolean gameEnd;
     private String situation;
+   
+   
     //constructor
     public GameData(int randomNum){
-        this.userNum=userNum;
+        this.randomNum=randomNum;
         inf=0;
         sup=100;
         round=0;
         situation="";
+        gameEnd=false;
       }
     
-    //ελεγχει αν ενα string περιεχει ενα char
-    private boolean containsNoChar(String str){
+    
+    
+    public void playGame(){
+            round=round+1;
+            testMatch(userNum); 
+            
+    }
+    public void testMatch(int userNum){
+        if(userNum==randomNum){
+            situation="επιτυχια";
+      }
+        else{
+            if (userNum>=randomNum){
+                situation="υψηλο";
+                sup=userNum-1;
+               }
+            else{
+                situation="χαμηλο";
+                inf=userNum+1;
+            }
+    }
+   }
+    public boolean answerNumError(String userAnswer){
+        
+        if (containsChar(userAnswer)) {                   //αν δεν περιεχει  char ειμαστε ενταξη να το μετατρεωουμε σε ιντ
+            this.userNum=Integer.parseInt(userAnswer);      
+            return (numBetweenLimits());                  //αν επιπλεον ο αριθμος ειναι αναμεσα στα ορια
+            
+        }
+        else return false;
+    
+    }
+    
+    
+       private boolean numBetweenLimits(){
+        
+        return (userNum>=inf && userNum<=sup);
+        
+    }
+        
+     //ελεγχει αν ενα string περιεχει ενα char
+    private boolean containsChar(String str){
          boolean containsError=false;
           if (str != null && !str.isEmpty()) {
            for (char c : str.toCharArray()) {
-            if (containsError = Character.isLetter(c)) {
+            if (containsError = Character.isLetter(c)) {     //προσοχη υπαρχει = οχι ==
                 break;
              }
          }
        }
-          return containsError;
+          return (containsError);
      }
+    
+    public boolean answerError(String str){
+        if (str.equals("ο")){
+            this.gameEnd=true;
+            return true;
+          }
+        else return str.equals("ν");
+    }
      
    
     
-    private String messege(String situation)
+    public String messege()
   {
       switch (situation)
     {
       case "χαμηλο":
       case "υψηλο":
-      return(userNum+"είναι"+situation+"Ξαναπροσπάθησε!Δώσε έναν αριθμό μεταξύ "+inf+"και"+sup);
-      
+          return(userNum+"είναι"+situation+"Ξαναπροσπάθησε!Δώσε έναν αριθμό μεταξύ "+inf+"και"+sup);
       case "επιτυχια":
-       return("Συγχαρητήρια! Βρήκες τον αριθμό μετά από"+round+"προσπάθειες! Σκορ:"+(10-round)+"Θα ήθελες να ξαναπαίξεις (ν/ο)?");
+          return("Συγχαρητήρια! Βρήκες τον αριθμό μετά από"+round+"προσπάθειες! Σκορ:"+(10-round)+"Θα ήθελες να ξαναπαίξεις (ν/ο)?");
+      case "σφαλμα":
+          return ("Μη αποδεκτη απαντηση");
       
       default:
           return("Δώσε έναν αριθμό μεταξυ 0και 100");
      }
     }
+
+    public boolean isGameEnd() {
+        return gameEnd;
+    }
+
+    public String getSituation() {
+        return situation;
+    }
+    
 }
     
 
